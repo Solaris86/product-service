a00b6d52743c3d71e474e7e9f1731a818d2bb39f
Solaris86 <nikola.suscevic@hotmail.com>
2021-12-15 00:05:23 +0100

    Initial project creation. Creation of package structure, dto, entity and repository classes.

---
+++ src/main/java/com/vinsguru/productservice/dto/ProductDto.java
@@ -0,0 +1,12 @@
+package com.vinsguru.productservice.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDto {
+
+    private String id;
+    private String description;
+    private String price;
+
+}
---
+++ src/main/java/com/vinsguru/productservice/entity/Product.java
@@ -0,0 +1,12 @@
+package com.vinsguru.productservice.entity;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+    private String id;
+    private String description;
+    private Integer price;
+
+}
